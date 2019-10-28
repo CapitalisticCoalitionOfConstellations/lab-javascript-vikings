1735deb0391ae34591bbbc1f693f52f2db41bc94
nicobared <nicobared@gmail.com>
2019-10-28 10:25:27 -0400

    should work

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,16 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength
+      }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{}
 
 // War
 class War {}
