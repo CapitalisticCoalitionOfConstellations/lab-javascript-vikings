3444475f9e8a67963153c280790b59c7f16f020f
Julian Phillips <julian.p@me.com>
2019-10-28 10:26:31 -0400

    Defined Soldier class and extended it to Viking and Saxon

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,24 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(incomingDamage) {
+        this.health -= incomingDamage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {}
 
 // War
 class War {}
